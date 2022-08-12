02b620d01230550a3928c500fb67ce68c5639620
Cristian Arboleda <doko.stan@gmail.com>
2022-08-11 21:32:04 -0500

    I created package structure

--- app/src/main/java/com/example/a4gamers/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/a4gamers/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.a4gamers
+package com.example.a4gamers.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.a4gamers.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
