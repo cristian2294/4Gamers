ef1925b8bd8dc1d7dca70148714567653b8ab8e0
Cristian Arboleda <doko.stan@gmail.com>
2022-08-24 23:23:11 -0500

    Working on the DetailView

--- app/src/main/java/com/example/forgamers/ui/view/GameDetailActivity.kt
+++ app/src/main/java/com/example/forgamers/ui/view/GameDetailActivity.kt
@@ -2,11 +2,14 @@ package com.example.forgamers.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.forgamers.data.model.Game
 import com.example.forgamers.databinding.ActivitylGameDetailBinding
-import com.example.forgamers.domain.GetGameDetailUseCase
 import com.example.forgamers.ui.viewmodel.GameDetailViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,20 +23,48 @@ class GameDetailActivity : AppCompatActivity() {
     // Init VM
     private val gameDetailViewModel : GameDetailViewModel by viewModels()
 
+    // UI variables
+    private lateinit var ivGameDetail : ImageView
+    private lateinit var tvGameName : TextView
+    private lateinit var tvGameGenre : TextView
+    private lateinit var tvGameDeveloper : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitylGameDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initUI()
+
         val intent = intent
         val gameId = intent.getStringExtra("gameId")!!
 
-        /*
+
         gameDetailViewModel.getGameDetail(gameId)
 
         // Create the observer which updates the UI.
         val gameObserver = Observer<Game> { game ->
+
+            tvGameName.text = game.title
+            tvGameGenre.text = game.genre
+            tvGameDeveloper.text = game.developer
+
+            val ulrImage = game.thumbnail
+            Glide.with(this)
+                .load(ulrImage)
+                .centerCrop()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(ivGameDetail)
         }
-       */
+
+        // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
+        gameDetailViewModel.gameModel.observe(this, gameObserver)
+    }
+
+    private fun initUI() {
+        ivGameDetail = binding.ivGameDetail
+        tvGameName = binding.tvGameDetailName
+        tvGameGenre = binding.tvGameDetailGenre
+        tvGameDeveloper = binding.tvGameDetailDeveloper
     }
 }
--- app/src/main/java/com/example/forgamers/ui/viewmodel/GameDetailViewModel.kt
+++ app/src/main/java/com/example/forgamers/ui/viewmodel/GameDetailViewModel.kt
@@ -5,11 +5,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.forgamers.data.model.Game
 import com.example.forgamers.domain.GetGameDetailUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
+@HiltViewModel
 class GameDetailViewModel @Inject constructor(
     private val getGameDetailUseCase: GetGameDetailUseCase
 ): ViewModel() {
